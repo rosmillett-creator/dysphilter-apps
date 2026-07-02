84cd8025a65569b2cff0740e770e72b6f46e99a7
rosmillett-creator <rosmillett18@googlemail.com>
2026-07-02 21:05:12 +0100

    PWA: Enef Car service worker

---
+++ sw-enef.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'enefcar-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icons/enef-192.png',
+  '/icons/enef-512.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function() {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.map(function(name) {
+        if (name !== CACHE_NAME) return caches.delete(name);
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request).then(function(resp) {
+        if (resp.status === 200 && e.request.method === 'GET') {
+          var respClone = resp.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(e.request, respClone).catch(function() {});
+          });
+        }
+        return resp;
+      }).catch(function() {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
